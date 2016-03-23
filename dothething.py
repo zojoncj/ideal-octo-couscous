16483094bbf50feb03e52ab6ecc8403684d156d0
Josh Zojonc <Josh.Zojonc@Oregonstate.edu>
2016-03-23 11:30:00 -0700

    more work

--- dothething.py
+++ dothething.py
@@ -5,15 +5,17 @@ import datetime
 import dateutil.relativedelta
 import sys, getopt
 
-
 today = datetime.date.today()
 d = (datetime.date(today.year,today.month,1) - dateutil.relativedelta.relativedelta(days=1))
 lastMonth = d.strftime("%B")
 
 #cols=['YUABCGEN',2,'3BC2', d.strftime("%d-%b-%Y")]
 
-def main(argv):
+cols=[]
+with open('cols.cfg') as f:
+  cols=f.read().splitlines()
 
+def main(argv):
     defaultIn = "nws.backups.%s.csv" % lastMonth
     defaultOut = "nws.backups.FUPLOAD.%s.csv" % lastMonth
     inputfile = defaultIn
@@ -37,11 +39,34 @@ def main(argv):
 
 def letsDoIt(i,o):
   total=0;
-  with open(i, 'rb') as csvfile:
-    csvfile.next()
-    reader = csv.reader(csvfile, delimiter=',')
+  with open(i, 'rb') as ifile:
+    ifile.next()
+    reader = csv.reader(ifile, delimiter=',')
     for row in reader:
-      total+=float(row[5][1:])
+      money = row[5][1:]
+      index=row[2].split(' ')
+      if 1 == len(index): index.append('')
+      
+      total+=float(money)
+      newrow=[cols[0],
+              '',
+              cols[1],
+              cols[2],
+              '',
+              d.strftime("%d-%b-%Y"),
+              money,
+              cols[3],
+              cols[4],
+              cols[5],
+              cols[6],
+              index[0],
+              '',
+              '',
+              cols[7],
+              '',
+              index[1]
+          ]
+      print newrow
       
 
 
